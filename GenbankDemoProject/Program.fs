3b2ce19483e6e2fcb4248f7f9eccaf805a319fe1
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-09-02 11:39:03 +1000

    :tada: Initial Commit

---
+++ GenbankDemoProject/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Genbank
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    printfn "%s" Genbank.Provider.Archaea.FTPUrl
+    printfn "%s" Genbank.Provider.Other.Genomes.``synthetic_Mycoplasma_genitalium_JCVI-1.0``.``Annotation Hashes``.``Assembly accession``
+    0 // return an integer exit code
