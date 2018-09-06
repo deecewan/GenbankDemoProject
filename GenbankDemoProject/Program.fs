293a5846fd1c7998153867f90d249c462758b6f3
David Buchan-Swanson <david.buchanswanson@gmail.com>
2018-09-06 21:57:13 +1000

    upgrade to the latest GTP library to fix compiler problem

--- GenbankDemoProject/Program.fs
+++ GenbankDemoProject/Program.fs
@@ -6,6 +6,7 @@ open Genbank
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    printfn "%s" Provider.Fungi.Genomes.``Cryptococcus_sp._JCM_24511``.``Annotation Hashes``.``Protein names last changed``
     printfn "%s" Genbank.Provider.Archaea.FTPUrl
     printfn "%s" Genbank.Provider.Other.Genomes.``synthetic_Mycoplasma_genitalium_JCVI-1.0``.``Annotation Hashes``.``Assembly accession``
     0 // return an integer exit code
